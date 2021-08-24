807035888230b3935b69d4dcce31b3da4a8777e2
gubiarpa <billy.arredondo@pucp.pe>
2021-08-23 22:19:58 -0500

    create header to image converter

---
+++ src/LearningApp/WpfTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of a drive, folder or file
+    /// </summary>
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
